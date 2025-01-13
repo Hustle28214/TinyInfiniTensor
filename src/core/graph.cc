723866f99e147d9e78d6361a37b48c362b9e713f
Leyan <86906700+Hustle28214@users.noreply.github.com>
2025-01-14 00:17:05 +0800

    Update graph.cc

--- src/core/graph.cc
+++ src/core/graph.cc
@@ -156,7 +156,7 @@ namespace infini
         size_t sizeTensorPtr = this->tensors.size();
         std::vector<size_t> offset;
         // getBytes()
-        for(auto tensor:tensors){
+        for(const auto tensor:tensors){
             offset.push_back(allocator.alloc(tensor->getBytes()));
         }
         auto dptr = this->allocator.getPtr();
