c59d237541c8da473e4e05cb4a3c7e9f2b7859c0
Leyan <86906700+Hustle28214@users.noreply.github.com>
2025-01-13 19:15:25 +0800

    Update allocator.cc

--- src/core/allocator.cc
+++ src/core/allocator.cc
@@ -1,8 +1,11 @@
 #include "core/allocator.h"
 #include <utility>
-
+#include <list>
+#include <algorithm>
+#include <cmath>
 namespace infini
 {
+    
     Allocator::Allocator(Runtime runtime) : runtime(runtime)
     {
         used = 0;
@@ -13,6 +16,7 @@ namespace infini
         // the longest data type currently supported by the DataType field of
         // the tensor
         alignment = sizeof(uint64_t);
+        
     }
 
     Allocator::~Allocator()
@@ -28,12 +32,41 @@ namespace infini
         IT_ASSERT(this->ptr == nullptr);
         // pad the size to the multiple of alignment
         size = this->getAlignedSize(size);
-
+        // 对齐大小
         // =================================== 作业 ===================================
         // TODO: 设计一个算法来分配内存，返回起始地址偏移量
         // =================================== 作业 ===================================
 
-        return 0;
+
+        // first fit
+        // 从可用内存块的开始位置顺序查找，找到第一个满足需求的内存块。
+        // 分配该内存块，如果有剩余空间，则将剩余部分标记为新的可用块。
+        auto it = freeBlockMap.begin();
+        for (; it != freeBlockMap.end(); ++it) {
+            if (it->second >= size) {
+                size_t start = it->first;
+
+                used += size;
+                peak = std::max(used, peak);
+
+                size_t remain = it->second - size;
+                if (remain > 0) {
+                    freeBlockMap[start + size] = remain;
+                }
+
+                freeBlockMap.erase(it);
+
+                return start;
+            }
+        }
+
+        // 如果没有找到，重新分配内存
+        size_t new_start = used;
+        used += size;
+        peak = std::max(peak, used);
+
+        return new_start;
+
     }
 
     void Allocator::free(size_t addr, size_t size)
@@ -44,7 +77,35 @@ namespace infini
         // =================================== 作业 ===================================
         // TODO: 设计一个算法来回收内存
         // =================================== 作业 ===================================
-    }
+    
+        used -= size;
+
+        // 将回收的内存块插入到 freeBlockMap 中
+        freeBlockMap[addr] = size;
+
+        // 合并相邻的空闲内存块
+        auto it = freeBlockMap.find(addr);
+        if (it != freeBlockMap.end()) {
+            auto prev_it = it;
+            if (prev_it != freeBlockMap.begin()) {
+                --prev_it;
+                if (prev_it->first + prev_it->second == addr) {
+                    prev_it->second += size;
+                    freeBlockMap.erase(it);
+                    it = prev_it;
+                }
+            }
+
+            auto next_it = it;
+            ++next_it;
+            if (next_it != freeBlockMap.end() && addr + size == next_it->first) {
+                it->second += next_it->second;
+                freeBlockMap.erase(next_it);
+            }
+        }
+        
+        
+        }
 
     void *Allocator::getPtr()
     {
@@ -67,3 +128,4 @@ namespace infini
                   << ", peak memory: " << this->peak << std::endl;
     }
 }
+
